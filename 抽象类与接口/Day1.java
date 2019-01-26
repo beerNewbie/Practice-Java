8f8109cf4f9d1df5d12d64456e5948d3334c5cad
Newbie <46735511+beerNewbie@users.noreply.github.com>
2019-01-26 21:58:59 +0800

    抽象类的使用

--- 抽象类与接口/Day1.java
+++ 抽象类与接口/Day1.java
@@ -1,3 +1,10 @@
+/**
+ *  1.所有的抽象类必须有子类。
+ *  2.抽象类的子类必须覆写抽象类的所有抽象方法（子类不是抽象类）
+ *  【方法覆写一定要考虑权限问题，权限尽量都用public】
+ *  3.抽象类的对象可以通过对象多态性利用子类为其实例化
+ *  4.private与abstract不能同时使用。
+*/
 abstract class Person {
     private String name;
     public String getName() {
