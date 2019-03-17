9d7a37812815b3eb260cb0965a0ae535846cdc32
beerNewbie <1479906877@qq.com>
2019-03-17 21:17:18 +0800

    [Update]<反射>2019/03/17

---
+++ 反射/src/www/hbc/test/TestDemo01.java
@@ -0,0 +1,47 @@
+package www.hbc.test;
+
+/**
+ * @Author: Beer
+ * @Date: 2019/3/17 16:06
+ * @Description:工厂模式与反射
+ */
+interface IComputer {
+    public void buyComputer() ;
+}
+class Mac implements IComputer {
+    @Override
+    public void buyComputer() {
+        System.out.println("秒一台苹果");
+    }
+}
+class Surface implements IComputer {
+    @Override
+    public void buyComputer() {
+        System.out.println("秒一台微软");
+    }
+}
+class ComuterFactoy {
+    private ComuterFactoy() {}
+    public static IComputer getInstance(String className) {
+        IComputer computer = null;
+        try {
+            computer = (IComputer) Class.forName(className).newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return computer;
+
+    }
+}
+
+public class TestDemo01 {
+    public static void main(String[] args) {
+        IComputer computer =
+                ComuterFactoy.getInstance("www.hbc.test.Mac");
+        computer.buyComputer();
+    }
+}
